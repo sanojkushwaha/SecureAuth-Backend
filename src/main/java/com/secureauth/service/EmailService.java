4f5182694fbef0b9014f0993213b93c74a9c84c6
Sanoj Kr. Kushwaha <kushawahasanoj123@gmail.com>
2026-09-10 16:03:20 +0530

    Refactor EmailService methods for clarity
    Refactor email service methods to improve readability and maintainability by formatting parameters and email body.

--- src/main/java/com/secureauth/service/EmailService.java
+++ src/main/java/com/secureauth/service/EmailService.java
@@ -18,31 +18,69 @@ public class EmailService {
     @Value("${app.frontend.url}")
     private String frontendUrl;
 
-    public void sendVerificationEmail(String toEmail, String fullName, String token) {
+    public void sendVerificationEmail(
+            String toEmail,
+            String fullName,
+            String token
+    ) {
+
         String link = frontendUrl + "/verify-email?token=" + token;
-        String body = "Hi " + fullName + ",\n\n"
-                + "Thanks for registering with SecureAuth. Please verify your email by clicking the link below:\n"
+
+        String body =
+                "Hi " + fullName + ",\n\n"
+                + "Thanks for registering with SecureAuth.\n\n"
+                + "Please verify your email by clicking the link below:\n\n"
                 + link + "\n\n"
-                + "This link expires in 24 hours.\n\n"
-                + "If you did not create this account, please ignore this email.";
-        send(toEmail, "Verify your email - SecureAuth", body);
+                + "This verification link expires in 24 hours.\n\n"
+                + "If you did not create this account, please ignore this email.\n\n"
+                + "Regards,\n"
+                + "SecureAuth Team";
+
+        send(
+                toEmail,
+                "Verify your email - SecureAuth",
+                body
+        );
     }
 
-    public void sendPasswordResetEmail(String toEmail, String fullName, String token) {
+    public void sendPasswordResetEmail(
+            String toEmail,
+            String fullName,
+            String token
+    ) {
+
         String link = frontendUrl + "/reset-password?token=" + token;
-        String body = "Hi " + fullName + ",\n\n"
-                + "We received a request to reset your password. Click the link below to set a new password:\n"
+
+        String body =
+                "Hi " + fullName + ",\n\n"
+                + "We received a request to reset your password.\n\n"
+                + "Click the link below to set a new password:\n\n"
                 + link + "\n\n"
-                + "This link expires in 1 hour. If you did not request this, you can safely ignore this email.";
-        send(toEmail, "Reset your password - SecureAuth", body);
+                + "This link expires in 1 hour.\n\n"
+                + "If you did not request this, you can safely ignore this email.\n\n"
+                + "Regards,\n"
+                + "SecureAuth Team";
+
+        send(
+                toEmail,
+                "Reset your password - SecureAuth",
+                body
+        );
     }
 
-    private void send(String to, String subject, String body) {
+    private void send(
+            String to,
+            String subject,
+            String body
+    ) {
+
         SimpleMailMessage message = new SimpleMailMessage();
+
         message.setFrom(fromAddress);
         message.setTo(to);
         message.setSubject(subject);
         message.setText(body);
+
         mailSender.send(message);
     }
 }
